d32915afadf31f4f96a939b5c5ba19064b25b44b
stas224 <stas224stas@gmail.com>
2023-02-22 20:19:12 +0300

    fourth commit

--- store/products/models.py
+++ store/products/models.py
@@ -12,7 +12,10 @@ class ProductCategory(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Продукт {self.name} | Категория {self.category.name}'
