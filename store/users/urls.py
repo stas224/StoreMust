3333309240a3a7dda733300d1dabc5ec04ed4770
stas224 <stas224stas@gmail.com>
2023-02-23 23:21:27 +0300

    5 commit

---
+++ store/users/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from users.views import login, registration
+app_name = 'users'
+urlpatterns = [
+    path('login', login, name='login'),
+    path('registration', registration, name='registration')
+]
