5a05637a1a61ac441f0696c289d8bc15dca886d3
stas224 <stas224stas@gmail.com>
2023-06-25 21:28:55 +0300

    add test for IndexView and ProductsListView

--- store/products/tests.py
+++ store/products/tests.py
@@ -1,3 +1,52 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from http import HTTPStatus
+
+from products.models import Product, ProductCategory
+
+
+class TestIndexView(TestCase):
+
+    def test_index(self):
+        path = reverse('index')
+        response = self.client.get(path)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'StoreMust')
+        self.assertTemplateUsed(response, 'products/index.html')
+
+
+class TestProductsListView(TestCase):
+    fixtures = ['categories.json', 'goods.json']
+
+    def setUp(self):
+        self.products = Product.objects.all()
+
+    def _common_test(self, response):
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'StoreMust - Каталог')
+        self.assertTemplateUsed(response, 'products/products.html')
+
+    def test_list(self):
+        path = reverse('products:index')
+        response = self.client.get(path)
+
+        self._common_test(response)
+
+        self.assertEqual(
+            list(response.context_data['object_list']),
+            list(self.products[:3])
+        )
+
+    def test_list_with_category(self):
+        category = ProductCategory.objects.first()
+        path = reverse('products:category', kwargs={'category_name': category.name})
+        response = self.client.get(path)
+
+        self._common_test(response)
+
+        self.assertEqual(
+            list(response.context_data['object_list']),
+            list(self.products.filter(category__name=category.name))
+        )
